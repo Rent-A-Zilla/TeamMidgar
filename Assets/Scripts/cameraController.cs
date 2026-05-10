49513a7406f96ab8af89bdbad3385b7943f5a902
Hunter Z <hunterziegler03@gmail.com>
2026-05-10 02:40:03 -0400

    Initial Unity project setup and added player movement and a camera controller. Also added a floor to test movement and fine tuned the movement a little bit

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] Transform player;
+
+    float camRotx;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sens;
+
+        camRotx -= mouseY;
+
+        camRotx = Mathf.Clamp(camRotx, lockVertMin, lockVertMax);
+        transform.localRotation = Quaternion.Euler(camRotx, 0 , 0);
+
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreLayer;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int springMod;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int jumpMax;
+    [SerializeField] int gravity;
+
+    int jumpCount;
+    int HPOrig;
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {   
+        if(controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel.y = 0;
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVel * Time.deltaTime);
+
+        playerVel.y -= gravity * Time.deltaTime;
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= springMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= springMod;
+        }
+    }
+
+    void jump()
+    {
+        if(Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+    }
+}
