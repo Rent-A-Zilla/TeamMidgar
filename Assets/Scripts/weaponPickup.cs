f5917b876047a874acb58c1858d6e36a2a21cbf4
Hunter Z <hunterziegler03@gmail.com>
2026-05-19 03:30:00 -0400

    Added pickupSpin Script Handles rotating weapon pickups
    Created reusable weapon pickup prefab/game object setup
    Added GunHolder system for displaying weapon visuals

--- Assets/Scripts/weaponPickup.cs
+++ Assets/Scripts/weaponPickup.cs
@@ -1,8 +1,20 @@
+using System;
 using UnityEngine;
 
 public class weaponPickup : MonoBehaviour
 {
     [SerializeField] GameObject weaponPrefab;
+    [SerializeField] Transform gunHolder;
+    [SerializeField] GameObject weaponVisual;
+
+
+    void Start()
+    {
+        GameObject visual = Instantiate(weaponVisual, gunHolder);
+
+        visual.transform.localPosition = Vector3.zero;
+        visual.transform.localRotation = Quaternion.identity;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
