9c3f6802bd3ad70419c7836de218135a2d719393
Serge <agounkeserge@gmail.com>
2026-05-23 11:42:39 -0600

    Add chain reaction behavior to rolling rocks

--- Assets/Scripts/Level Design/rollingRockTrigger.cs
+++ Assets/Scripts/Level Design/rollingRockTrigger.cs
@@ -1,9 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class RollingRockTrigger : MonoBehaviour
 {
+    [Header("Rocks To Release")]
     public RollingRock[] rocksToRelease;
+
+    [Header("Trigger Settings")]
     public bool triggerOnlyOnce = true;
+
+    [Header("Chain Reaction Settings")]
+    public bool useChainReaction = true;
+    public float delayBetweenRocks = 0.5f;
 
     private bool hasTriggered;
 
@@ -17,6 +25,31 @@ public class RollingRockTrigger : MonoBehaviour
 
         hasTriggered = true;
 
+        if (useChainReaction)
+        {
+            StartCoroutine(ReleaseRocksOneByOne());
+        }
+        else
+        {
+            ReleaseAllRocksAtOnce();
+        }
+    }
+
+    private IEnumerator ReleaseRocksOneByOne()
+    {
+        foreach (RollingRock rock in rocksToRelease)
+        {
+            if (rock != null)
+            {
+                rock.StartRolling();
+            }
+
+            yield return new WaitForSeconds(delayBetweenRocks);
+        }
+    }
+
+    private void ReleaseAllRocksAtOnce()
+    {
         foreach (RollingRock rock in rocksToRelease)
         {
             if (rock != null)
