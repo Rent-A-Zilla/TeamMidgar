b10c73738c1a6d535fb7f86dcce33dfcc5fdbbc4
Aterilus <terilusartavius@gmail.com>
2026-05-25 19:10:59 -0400

    Implemented Checkpoint and player spawn position

---
+++ Assets/Scripts/Player Stuff/checkPoint.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class checkPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && gameManager.instance.playerStartPos.transform.position != transform.position)
+        {
+            gameManager.instance.player.transform.position = transform.position;
+            StartCoroutine(displayCheckPointUI());
+        }
+    }
+
+    IEnumerator displayCheckPointUI()
+    {
+        gameManager.instance.checkpointPopup.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        gameManager.instance.checkpointPopup.SetActive(false);
+    }
+}
