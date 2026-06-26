0cf76ff53afd4ce1b170a7c7fb68b594642acef5
Garrett Kondratowicz <garrett.m.kondratowicz@gmail.com>
2026-06-26 16:56:18 -0400

    Fixed corridors not connecting some rooms. Now the player should be able to get to every room generated.

--- Assets/Scripts/Level Generation/CorridorsGenerator.cs
+++ Assets/Scripts/Level Generation/CorridorsGenerator.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using UnityEngine;
 public class CorridorsGenerator
 {
     public List<Node> CreateCorridor(List<RoomNode> allNodesCollection, int corridorWidth)
@@ -17,10 +17,46 @@ public class CorridorsGenerator
             {
                 continue;
             }
-            CorridorNode corridor = new CorridorNode(node.ChildrenNodeList[0], node.ChildrenNodeList[1], corridorWidth);
+            (Node leftRoom, Node rightRoom) =
+    GetClosestRooms(node.ChildrenNodeList[0],node.ChildrenNodeList[1]);
+
+            CorridorNode corridor = new CorridorNode(leftRoom, rightRoom,corridorWidth);
+
             corridorList.Add(corridor);
         }
         return corridorList;
     }
+    private (Node, Node) GetClosestRooms(Node leftParent, Node rightParent)
+    {
+        List<Node> leftRooms = StructureHelper.TraverseGraphToExtractLowestLeafes(leftParent);
+        List<Node> rightRooms = StructureHelper.TraverseGraphToExtractLowestLeafes(rightParent);
 
+        float shortestDistance = float.MaxValue;
+
+        Node bestLeft = null;
+        Node bestRight = null;
+
+        foreach (Node left in leftRooms)
+        {
+            foreach (Node right in rightRooms)
+            {
+                Vector2 leftCenter =
+                    (left.BottemLeftAreaCorner + left.TopRightAreaCorner) / 2;
+
+                Vector2 rightCenter =
+                    (right.BottemLeftAreaCorner + right.TopRightAreaCorner) / 2;
+
+                float dist = Vector2.Distance(leftCenter, rightCenter);
+
+                if (dist < shortestDistance)
+                {
+                    shortestDistance = dist;
+                    bestLeft = left;
+                    bestRight = right;
+                }
+            }
+        }
+
+        return (bestLeft, bestRight);
+    }
 }
